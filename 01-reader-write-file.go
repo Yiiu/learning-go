6e3eb538988436c0aaf8f8b92c9e3039bf3e65c2
yiiu <yu.gintoki@gmail.com>
2018-04-03 18:00:08 +0800

    update

---
+++ 01-reader-write-file.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+	"log"
+)
+
+const (
+	filepath = "./01-reader-write-file.go"
+)
+
+func main() {
+	f, _ := os.Open(filepath)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+	f, err := os.Create("./extras/output.txt")
+	if err != nil {
+		log.Fatalln("Error creating file: ", err)
+	}
+	defer f.Close()
+
+	for _, str := range []string{"apple\n", "banana", "carrot"} {
+		bytes, err := f.WriteString(str)
+		if err != nil {
+			log.Fatalln("插入失败", err)
+		}
+		fmt.Printf("Wrote %d bytes to file\n", bytes)
+	}
+}
